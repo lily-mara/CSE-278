6ab7e7b9749b41c611cddbbedac5bc13b6d863b2
Nate Mara <natemara@gmail.com>
2015-02-02 16:52:26 -0500

    write calc2 function for calculating age relative to 2050

--- lab_02/lab2.s
+++ lab_02/lab2.s
@@ -1,11 +1,18 @@
 	.file	"lab2.c"
 	.globl	year
+	.globl	year2
 	.data
 	.align 4
 	.type	year, @object
 	.size	year, 4
+	.type	year2, @object
+	.size	year2, 4
 year:
 	.long	2020
+	.section	.rodata
+	.align 8
+year2:
+	.long	2050
 	.section	.rodata
 	.align 8
 .LC0:
@@ -39,11 +46,22 @@ main:
 	movl	$.LC2, %edi
 	movl	$0, %eax
 	call	printf
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	calc_age2
+	movl	%eax, %edx
+	movl	year2(%rip), %eax
+	movl	%eax, %esi
+	movl	$.LC2, %edi
+	movl	$0, %eax
+	call	printf
 	leave
 	ret
 	.size	main, .-main
 	.globl	calc_age
 	.type	calc_age, @function
+	.globl	calc_age2
+	.type	calc_age2, @function
 calc_age:
 	pushq	%rbp
 	movq	%rsp, %rbp
@@ -53,5 +71,15 @@ calc_age:
 	popq	%rbp
 	ret
 	.size	calc_age, .-calc_age
+	.ident	"GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
+calc_age2:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	%edi, -4(%rbp)
+	movl	year2(%rip), %eax
+	subl	-4(%rbp), %eax
+	popq	%rbp
+	ret
+	.size	calc_age2, .-calc_age2
 	.ident	"GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
 	.section	.note.GNU-stack,"",@progbits
