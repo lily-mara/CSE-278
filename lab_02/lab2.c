94ded3fd2c4032ffcc76a10c94f3aa7b21c03e58
Nate Mara <natemara@gmail.com>
2015-02-02 16:21:58 -0500

    write program in c

--- lab_02/lab2.c
+++ lab_02/lab2.c
@@ -1,3 +1,17 @@
+#include<stdio.h>
+
+int year = 2020;
+int calc_age(int);
+
 int main() {
-	return 0;
+	int birth_year;
+
+	printf("Enter the year you were born: ");
+	scanf("%d", &birth_year);
+
+	printf("In %d, you will be %d years old\n", year, calc_age(birth_year));
+}
+
+int calc_age(int birth_year) {
+	return year - birth_year;
 }
