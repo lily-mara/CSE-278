9fb7c049740ed934e8126fc47d43f7a74f0a1c67
Nate Mara <natemara@gmail.com>
2015-02-02 17:05:42 -0500

    re-generate asm file from .s file

--- lab_02/lab2.asm
+++ lab_02/lab2.asm
@@ -1,97 +1,86 @@
         ;FILE "lab2.c"
+        GLOBAL  year
+        GLOBAL  year2
+        SECTION .data
+        ALIGN 4
+        GLOBAL year:object
+        ;.size  year, 4
+        GLOBAL year2:object
+        ;.size  year2, 4
+year: 
+        dd      2020
+        SECTION         .rodata
+        ALIGN 8
+year2: 
+        dd      2050
+        SECTION         .rodata
+        ALIGN 8
+.LC0: 
+        db      'Enter the year you were born: '
+.LC1: 
+        db      '%d'
+        ALIGN 8
+.LC2: 
+        db      'In %d, you will be %d years old',10,''
         SECTION .text
         GLOBAL  main
-        ALIGN 16, db 090h
         GLOBAL main:function
-main:                                    ; @main
-MISMATCH: "        .cfi_startproc"
-; BB#0:
+main: 
 MISMATCH: "        pushq   %rbp"
-.Ltmp2:
-MISMATCH: "        .cfi_def_cfa_offset 16"
-.Ltmp3:
-MISMATCH: "        .cfi_offset %rbp, -16"
         movq    rbp,rsp
-.Ltmp4:
-MISMATCH: "        .cfi_def_cfa_register %rbp"
-MISMATCH: "        subq    $32, %rsp"
-MISMATCH: "        leaq    .L.str, %rdi"
-        mov     al,0
-MISMATCH: "        callq   printf"
-MISMATCH: "        leaq    .L.str1, %rdi"
-MISMATCH: "        leaq    -4(%rbp), %rsi"
-        mov     [rbp-8],eax             ; 4-byte Spill
-        mov     al,0
-MISMATCH: "        callq   __isoc99_scanf"
-        mov     esi, [year]
-        mov     edi, [rbp-4]
-        mov     [rbp-12],eax            ; 4-byte Spill
-        mov     [rbp-16],esi            ; 4-byte Spill
-MISMATCH: "        callq   calc_age"
-MISMATCH: "        leaq    .L.str2, %rdi"
-        mov     esi, [rbp-16]           ; 4-byte Reload
+MISMATCH: "        subq    $16, %rsp"
+        mov     edi, .LC0
+        mov     eax,0
+        call    printf
+MISMATCH: "        leaq    -4(%rbp), %rax"
+        movq    rsi,rax
+        mov     edi, .LC1
+        mov     eax,0
+        call    __isoc99_scanf
+        mov     eax, [rbp-4]
+        mov     edi,eax
+        call    calc_age
         mov     edx,eax
-        mov     al,0
-MISMATCH: "        callq   printf"
-        mov     edx,0
-        mov     [rbp-20],eax            ; 4-byte Spill
-        mov     eax,edx
-MISMATCH: "        addq    $32, %rsp"
-MISMATCH: "        popq    %rbp"
+        mov     eax, [year(%rip)]
+        mov     esi,eax
+        mov     edi, .LC2
+        mov     eax,0
+        call    printf
+        mov     eax, [rbp-4]
+        mov     edi,eax
+        call    calc_age2
+        mov     edx,eax
+        mov     eax, [year2(%rip)]
+        mov     esi,eax
+        mov     edi, .LC2
+        mov     eax,0
+        call    printf
+        leave
         ret
-.Ltmp5:
-        GLOBAL  main:function (.Ltmp5-main)
-MISMATCH: "        .cfi_endproc"
-
+        GLOBAL  main:function (.-main)
         GLOBAL  calc_age
-        ALIGN 16, db 090h
         GLOBAL calc_age:function
-calc_age:                                ; @calc_age
-MISMATCH: "        .cfi_startproc"
-; BB#0:
+        GLOBAL  calc_age2
+        GLOBAL calc_age2:function
+calc_age: 
 MISMATCH: "        pushq   %rbp"
-.Ltmp8:
-MISMATCH: "        .cfi_def_cfa_offset 16"
-.Ltmp9:
-MISMATCH: "        .cfi_offset %rbp, -16"
         movq    rbp,rsp
-.Ltmp10:
-MISMATCH: "        .cfi_def_cfa_register %rbp"
         mov     [rbp-4],edi
-        mov     edi, [year]
-        sub     edi, [rbp-4]
-        mov     eax,edi
+        mov     eax, [year(%rip)]
+        sub     eax, [rbp-4]
 MISMATCH: "        popq    %rbp"
         ret
-.Ltmp11:
-        GLOBAL  calc_age:function (.Ltmp11-calc_age)
-MISMATCH: "        .cfi_endproc"
-
-        GLOBAL year:object              ; @year
-        SECTION .data
-        GLOBAL  year
-        ALIGN 4
-year:
-        dd      2020                    ; 0x7e4
-        ;.size  year, 4
-
-        GLOBAL .L.str:object            ; @.str
-MISMATCH: "        .section        .rodata.str1.1,"aMS",@progbits,1"
-.L.str:
-        db      'Enter the year you were born: ', 0
-        ;.size  .L.str, 31
-
-        GLOBAL .L.str1:object           ; @.str1
-.L.str1:
-        db      '%d', 0
-        ;.size  .L.str1, 3
-
-        GLOBAL .L.str2:object           ; @.str2
-.L.str2:
-        db      'In %d, you will be %d years old\n', 0
-        ;.size  .L.str2, 33
-
-
-        ;IDENT "Ubuntu clang version 3.4-1ubuntu3 (tags/RELEASE_34/final) (based on LLVM 3.4)"
-MISMATCH: "        .section        ".note.GNU-stack","",@progbits"
+        GLOBAL  calc_age:function (.-calc_age)
+        ;IDENT "GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
+calc_age2: 
+MISMATCH: "        pushq   %rbp"
+        movq    rbp,rsp
+        mov     [rbp-4],edi
+        mov     eax, [year2(%rip)]
+        sub     eax, [rbp-4]
+MISMATCH: "        popq    %rbp"
+        ret
+        GLOBAL  calc_age2:function (.-calc_age2)
+        ;IDENT "GCC: (Ubuntu 4.8.2-19ubuntu1) 4.8.2"
+MISMATCH: "        .section        .note.GNU-stack,"",@progbits"
 
