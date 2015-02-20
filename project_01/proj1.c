5479f9127fd5da24e60e65325e24f3c057d83d14
Nate Mara <natemara@gmail.com>
2015-02-20 17:27:35 -0500

    move forward declarations and test code to header file

--- project_01/proj1.c
+++ project_01/proj1.c
@@ -2,33 +2,8 @@
 #include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
-#include<assert.h>
 
-typedef enum { INT, FLOAT, TYPE_ERROR } type_t;
-typedef enum { ADD, SUB, DIV, MULT, OP_ERROR } op_t;
-
-bool valid_binary(const char*);
-type_t parse_type_choice(const char*);
-op_t parse_op_choice(const char*);
-int power(int, int);
-int bin_to_dec(char*);
-bool is_whitespace(char);
-char* trim_whitespace(char*);
-char* get_str();
-char* get_binary(char*);
-type_t get_type();
-op_t get_op();
-void run_int(char*, char*, op_t);
-void run_float(char*, char*, op_t);
-int bin2int(char*);
-float bin2float(char*);
-void test_bin2int();
-char* int2bin(int);
-char* float2bin(float);
-char* bin2hex(char*);
-char* bin2oct(char*);
-void test_int2bin();
-char oct2ascii(char*);
+#include "main.h"
 
 int main() {
 	char *num1 = get_binary("first");
@@ -52,36 +27,6 @@ int main() {
 	free(num2);
 
 	printf("Goodbye!\n");
-}
-
-
-void test_bin2int() {
-	assert(bin2int("00000000") == 0);
-	assert(bin2int("01111111") == 127);
-	assert(bin2int("01111110") == 126);
-	assert(bin2int("00000010") == 2);
-	assert(bin2int("00000001") == 1);
-	assert(bin2int("00000000") == 0);
-	assert(bin2int("11111111") == -1);
-	assert(bin2int("11111110") == -2);
-	assert(bin2int("10000010") == -126);
-	assert(bin2int("10000001") == -127);
-	assert(bin2int("10000000") == -128);
-	assert(bin2int("11111111111111111111111111111111") == -1);
-	assert(bin2int("11111111111111111111111111110100") == -12);
-}
-
-void test_int2bin() {
-	assert(strcmp("00000000000000000000000000000000", int2bin(0)) == 0);
-	assert(strcmp("00000000000000000000000001111111", int2bin(127)) == 0);
-	assert(strcmp("00000000000000000000000001111110", int2bin(126)) == 0);
-	assert(strcmp("00000000000000000000000000000010", int2bin(2)) == 0);
-	assert(strcmp("00000000000000000000000000000001", int2bin(1)) == 0);
-	assert(strcmp("11111111111111111111111111111111", int2bin(-1)) == 0);
-	assert(strcmp("11111111111111111111111111111110", int2bin(-2)) == 0);
-	assert(strcmp("11111111111111111111111110000010", int2bin(-126)) == 0);
-	assert(strcmp("11111111111111111111111110000001", int2bin(-127)) == 0);
-	assert(strcmp("11111111111111111111111110000000", int2bin(-128)) == 0);
 }
 
 void run_int(char* str1, char* str2, op_t op) {
@@ -249,6 +194,9 @@ float bin2float(char* input) {
 
 	strncpy(exponent_str, input + 1, 8);
 	strncpy(significand_str, input + 1, 8);
+
+	free(exponent_str);
+	free(significand_str);
 
 	return 0;
 }
