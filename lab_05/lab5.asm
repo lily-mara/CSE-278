6c1d2453573b8fc9ca872ea03aa6521b92ad62bc
Nate Mara <natemara@gmail.com>
2015-03-02 16:52:31 -0500

    solve lab

--- lab_05/lab5.asm
+++ lab_05/lab5.asm
@@ -2,7 +2,8 @@ global main
 extern printf, scanf
 
 section .data
-	input: dd 0
+	num1: dd 0
+	num2: dd 0
 	fmtMsg: db 'Enter input', 0xA, 0x0
 	fmtPrintf: db 'result = %d', 0xA, 0x0
 	fmtScanf: db '%d', 0x0
@@ -13,13 +14,25 @@ main:
 	call printf
 	add esp, 4
 
-	mov eax, input
+	mov eax, num1
 	push eax
 	push fmtScanf
 	call scanf
 	add esp, 8
 
-	push dword [input]
+	push fmtMsg
+	call printf
+	add esp, 4
+
+	mov eax, num2
+	push eax
+	push fmtScanf
+	call scanf
+	add esp, 8
+
+	mov eax, [num1]
+	add eax, [num2]
+	push dword eax
 	push fmtPrintf
 	call printf
 	add esp, 8
