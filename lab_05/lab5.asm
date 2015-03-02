7e4b1a9d631991cdb0fad085d587cb51da6d3b79
Nate Mara <natemara@gmail.com>
2015-03-02 16:37:02 -0500

    add basic lab 5 files

---
+++ lab_05/lab5.asm
@@ -0,0 +1,25 @@
+global main
+extern printf, scanf
+
+section .data
+	input: dd 0
+	fmtMsg: db 'Enter input', 0xA, 0x0
+	fmtPrintf: db 'result = %d', 0xA, 0x0
+	fmtScanf: db '%d', 0x0
+
+section .text
+	push fmtMsg
+	call printf
+	add esp, 4
+
+	mov eax, [input]
+	push eax
+	push fmtScanf
+	call scanf
+	add esp, 8
+
+	push dword [input]
+	push fmtPrintf
+	call printf
+	add esp, 8
+	ret
